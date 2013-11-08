c5e2f2dd638660c5b6711f09b81780cbe3a75b39
pennpanda <pennpanda@pennpanda-32-v2.(none)>
2013-11-08 10:45:58 -0500

    mit-os

---
+++ hw-lab1/lab1/conf/env.mk
@@ -0,0 +1,17 @@
+# env.mk - configuration variables for the JOS lab
+
+
+# '$(V)' controls whether the lab makefiles print verbose commands (the
+# actual shell commands run by Make), as well as the "overview" commands
+# (such as '+ cc lib/readline.c').
+#
+# For overview commands only, the line should read 'V = @'.
+# For overview and verbose commands, the line should read 'V ='.
+V = @
+
+#uncomment if use standard tool chain
+GCCPREFIX=
+
+# '$(HANDIN_EMAIL)' is the email address to which lab handins should be
+# sent.
+HANDIN_EMAIL = 6.828-handin@pdos.lcs.mit.edu
